3a8d66c56562c37687e53c5af6346987d192ecb3
anurag <anurag9802@gmail.com>
2023-02-25 13:28:21 +0530

    added 2 containers

---
+++ client/src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Landing } from './landing/Landing';
+export { default as Avatar } from './avatar/Avatar';
+export { default as Ledger } from './ledger/Ledger';
+export { default as PastLedger } from './pastLedger/PastLedger';
+
+
