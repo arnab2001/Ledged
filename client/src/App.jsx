9c8aa69828b0c9021aad8784b6fc2029b6a71b9c
bishakhne0gi <bneogi102002@gmail.com>
2024-03-21 14:27:48 +0530

    vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,6 @@ import { Landing, Ledger, PastLedger } from "./containers";
 const App = () => {
   return (
     <>
-      {/* <Landing/> */}
-      {/* <Avatar/> */}
-      {/* <Ledger/> */}
-      {/* <PastLedger/> */}
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/profile" element={<Avatar />} />
-          <Route path="/past-ledger" element={<PastLedger />} />
-          <Route path="/create-ledger" element={<Ledger />} />
-          <Route path="/*" element={<Landing />} />
-        </Routes>
-      </BrowserRouter> */}
       <Landing />
     </>
   );
